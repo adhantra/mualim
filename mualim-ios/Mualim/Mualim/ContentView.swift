0f2f04a498f98d3975330a9e490ace5a9db7d63b
Agung Ramadhan Putra <agung.mizor@gmail.com>
2020-03-31 15:16:59 +0700

    first iOS project

---
+++ mualim-ios/Mualim/Mualim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mualim
+//
+//  Created by Agung Ramadhan Putra on 31/03/20.
+//  Copyright © 2020 Agung Ramadhan Putra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
